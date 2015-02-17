a97290b4ae896b719c685b4943544fea65432afe
Jordan Collier <jordanicollier@gmail.com>
2015-02-17 09:37:16 -0700

    answer warmup problems

--- celebrities.rb
+++ celebrities.rb
@@ -3,42 +3,44 @@ require_relative "testing_library"
 celebs = ["Britney", "Miley", "Madonna", "Cher", "Bruno", "Michael"]
 
 def how_many_celebrities(celebs)
-
+  celebs.count
 end
 
 
 def name_lengths(celebs)
-
+  celebs.map(&:length)
 end
 
 
 def reverse_celebrity_names(celebs)
-
+  celebs.map(&:reverse)
 end
 
 
 def first_three_celebrities_with_each(celebs)
-
+  first_three = []
+  celebs.each { |x| first_three << x if three.size < 3 }
+  three
 end
 
 
 def first_three_celebrities_without_each(celebs)
-
+  celebs[0..2]
 end
 
 
 def reverse_case_celebrities_names(celebs)
-
+  celebs.map { |x| x.swapcase}
 end
 
 
 def sum_of_all_celebrity_name_lengths(celebs)
-
+  celebs.inject(0) {|sum, x| sum + x.length }
 end
 
 
 def celebrities_with_long_names(celebs)
-
+ celebs.map { |x| x.length > 6 }
 end
 
 
